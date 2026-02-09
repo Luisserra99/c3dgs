9cdac6619c7a2b93cac3c373c92ab00ee4967721
Luisserra99 <luisserra99@gmail.com>
2026-02-09 09:46:49 -0300

    softmax

--- run_compression2.sh
+++ run_compression2.sh
@@ -12,7 +12,7 @@ for var in "${vars[@]}"; do
     python compress.py \
       --model_path "/d01/luis/datasets/models/$var" \
       --data_device "cuda" \
-      --output_vq "/d01/luis/compare_images_hilbert_10/$var/"
+      --output_vq "/d01/luis/outputs_preliminares_softmax/$var/"
       
     echo "Finished: $var"
     echo "-----------------------------------"
